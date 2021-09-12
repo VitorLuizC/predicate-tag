6d1bd2feaaf677f4922d10e91cddac75f1f8b4e2
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-09-12 00:02:58 -0300

    ✨ export Predicate and Refinement from bitty

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,41 @@
-import { isNumberParseable } from './';
+import type { Predicate, Refinement } from './index.js';
 
-describe('unit | isNumberParseable', () => {
-  it('returns `true` for values parseable number', () => {
-    expect(isNumberParseable('-7.5')).toBe(true);
-    expect(isNumberParseable(false)).toBe(true);
-    expect(isNumberParseable(1892)).toBe(true);
+describe('Predicate | type | unit test', () => {
+  it('predicate is a function that received a value and returns boolean', () => {
+    const isEven: Predicate<number> = (value) => value % 2 === 0;
+
+    expect(isEven(2)).toBe(true);
+    expect(isEven(1)).toBe(false);
+  });
+});
+
+describe('Refinement | type | unit test', () => {
+  function isString(value: unknown): value is string {
+    return typeof value === 'string';
+  }
+
+  it('is also a predicate', () => {
+    const refinement: Refinement<unknown, string> = isString;
+
+    const predicate: Predicate<unknown> = isString;
+
+    expect(refinement).toBe(predicate);
   });
 
-  it('returns `false` for values non parseable to number', () => {
-    expect(isNumberParseable('A8sa')).toBe(false);
-    expect(isNumberParseable({})).toBe(false);
-    expect(isNumberParseable(NaN)).toBe(false);
-    expect(isNumberParseable('18L')).toBe(false);
+  it('is a function that refines value type', () => {
+    // refines from 'unknown' to 'string'.
+    const refinement: Refinement<unknown, string> = isString;
+
+    const value: unknown = 'okay';
+
+    // @ts-expect-error because value is 'unknown' here.
+    expect(value.toUpperCase()).not.toBe('FAIL');
+
+    if (refinement(value)) {
+      // TS knows that value is a 'string' here. It was refined.
+      expect(value.toUpperCase()).toBe('OKAY');
+    } else {
+      fail();
+    }
   });
 });
